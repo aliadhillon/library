603489f382e8a191c84114c6a3ec873a089be688
Ali A. Dhillon <ali.dhillon102@gmail.com>
2020-03-26 00:19:20 +0500

    Creates index and show actions and use names routes.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,13 +7,34 @@ use App\Http\Requests\PostRequest;
 
 class BookController extends Controller
 {
+    public function index()
+    {
+        return Book::all();
+    }
+
+    public function show(Book $book)
+    {
+        return $book;
+    }
+
     public function store(PostRequest $request)
     {
-        Book::create($request->validated());
+        $book = Book::create($request->validated());
+
+        return redirect()->route('books.show', ['book' => $book]);
     }
 
     public function update(PostRequest $request, Book $book)
     {
         $book->update($request->validated());
+
+        return redirect()->route('books.show', ['book' => $book]);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return redirect()->route('books.index');
     }
 }
