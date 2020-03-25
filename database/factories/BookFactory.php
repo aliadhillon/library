cebc5fdfa69c07baf659a7b3048c9acd49b3398d
Ali A. Dhillon <ali.dhillon102@gmail.com>
2020-03-26 04:05:13 +0500

    Update BookFactory.php
    to reflect author to author_id

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,12 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Author;
 use App\Book;
 use Faker\Generator as Faker;
 
 $factory->define(Book::class, function (Faker $faker) {
     return [
         'title' => $faker->words(3, true),
-        'author' => $faker->name,
+        'author_id' => factory(Author::class),
     ];
 });
