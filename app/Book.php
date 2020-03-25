d10fd4e650761c5886d19af7b245ab5fdacf20df
Ali A. Dhillon <ali.dhillon102@gmail.com>
2020-03-26 04:03:49 +0500

    Implements auto author save
    Here I created teh author realtion. And created a mutator to serach for the given author title if found assign it's id to book otherwise create a new one and assign it's id to book.

--- app/Book.php
+++ app/Book.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $guarded = [];
+
+    public function setAuthorIdAttribute($authorName)
+    {
+        $author = Author::firstOrCreate([
+            'name' => $authorName,
+        ]);
+
+        $this->attributes['author_id'] = $author->id;
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
