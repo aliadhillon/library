5f78c37f3023cfa4642b80286508d1451e0a39d5
Ali A. Dhillon <ali.dhillon102@gmail.com>
2020-03-28 20:40:27 +0500

    Adds index and create in AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,8 +7,20 @@ use App\Http\Requests\AuthorRequest;
 
 class AuthorController extends Controller
 {
+    public function index()
+    {
+        return view('authors.index', ['authors' => Author::all()]);
+    }
+
+    public function create()
+    {
+        return view('authors.create');
+    }
+
     public function store(AuthorRequest $request)
     {
-        Author::create($request->validated());
+        $author = Author::create($request->validated());
+
+        return redirect()->route('authors.index')->with('msg', "{$author->name} has been created.");
     }
 }
